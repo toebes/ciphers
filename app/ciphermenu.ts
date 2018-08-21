ff347645cf2f4110fcf93a23b5beb7f8cd2a8f33
jtoebes <john@toebes.com>
2018-08-20 22:49:43 -0400

    Reorder menu to make Codebusters Test tools first - Part of #39

--- app/ciphermenu.ts
+++ app/ciphermenu.ts
@@ -26,22 +26,6 @@ export const CipherMenu: menuItem[] = [
         ]
     },
     {
-        title: "Other Assistants",
-        menu: [
-            { title: "Aristocrat/Patristocrat Solving Assistant", href: "Solver.html" },
-            { title: "Morbit Solving Assistant", href: "MorbitSolver.html", },
-            { title: "Fractionated Morse Solving Assistant", href: "FractionatedMorseSolver.html", },
-            { title: "Checkerboard Solving Assistant", href: "CheckerboardSolver.html", },
-            { title: "Xenocrypt Solving Assistant", href: "XenocryptSolver.html", },
-            { title: "Vigen&egrave;re Family Solving Assistant", href: "VigenereSolver.html", },
-            { title: "Gromark Solving Assistant", href: "GromarkSolver.html", },
-            { title: "Cryptarithm Solving Assistant", href: "CryptarithmSolver.html", },
-            { title: "Checkerboard Solving Assistant", href: "CheckerboardSolver.html", },
-            { title: "Ragbaby Solving Assistant", href: "RagbabySolver.html", },
-            { title: "Railfence/Redefence Solving Assistant", href: "RailfenceSolver.html", },
-        ]
-    },
-    {
         title: "Codebusters Test Tools",
         menu: [
             { title: "Test Manager", href: "TestManage.html", },
@@ -60,6 +44,22 @@ export const CipherMenu: menuItem[] = [
             { title: "Vigen&egrave;re", href: "VigenereEncrypt.html", cipherType: ICipherType.Vigenere, },
             { title: "Running Key", href: "RunningKeyEncoder.html", cipherType: ICipherType.RunningKey, },
             // { title: "Language Template Processor", href: "GenLanguage.html", },
+        ]
+    },
+    {
+        title: "Other Assistants",
+        menu: [
+            { title: "Aristocrat/Patristocrat Solving Assistant", href: "Solver.html" },
+            { title: "Morbit Solving Assistant", href: "MorbitSolver.html", },
+            { title: "Fractionated Morse Solving Assistant", href: "FractionatedMorseSolver.html", },
+            { title: "Checkerboard Solving Assistant", href: "CheckerboardSolver.html", },
+            { title: "Xenocrypt Solving Assistant", href: "XenocryptSolver.html", },
+            { title: "Vigen&egrave;re Family Solving Assistant", href: "VigenereSolver.html", },
+            { title: "Gromark Solving Assistant", href: "GromarkSolver.html", },
+            { title: "Cryptarithm Solving Assistant", href: "CryptarithmSolver.html", },
+            { title: "Checkerboard Solving Assistant", href: "CheckerboardSolver.html", },
+            { title: "Ragbaby Solving Assistant", href: "RagbabySolver.html", },
+            { title: "Railfence/Redefence Solving Assistant", href: "RailfenceSolver.html", },
         ]
     },
 ]

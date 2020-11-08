6300888f12676902057eaa9ce49d98af1112f4f2
root <root@John.localdomain>
2020-11-07 22:15:49 -0500

    Update lint options, upgrade NPM packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,6 @@ module.exports = {
         '@typescript-eslint/camelcase': 'off',
         'class-name-casing': 'off',
         '@typescript-eslint/class-name-casing': 'off',
-        '@typescript-eslint/no-empty-function': { allow: ['methods'] },
+        '@typescript-eslint/no-empty-function': 'off',
     },
 };

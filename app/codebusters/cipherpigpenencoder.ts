0b8deb1b3324a9a34332ed7c946164fda9f212ee
John Toebes <john@toebes.com>
2023-12-31 15:43:15 -0500

    Fix #435

--- app/codebusters/cipherpigpenencoder.ts
+++ app/codebusters/cipherpigpenencoder.ts
@@ -92,36 +92,27 @@ export class CipherPigPenEncoder extends CipherEncoder {
     }
     /**
       * Generate the recommended score and score ranges for a cipher
-      * @returns Computed score ranges for the cipher
+     * @returns Computed score ranges for the cipher and text description
       */
-    public genScoreRange(): suggestedData {
+    public genScoreRangeAndText(): suggestedData {
         const qdata = this.analyzeQuote(this.state.cipherString)
         let suggested = Math.round(qdata.unique * 1.5 + qdata.len)
         const min = Math.max(suggested - 5, 0)
         const max = suggested + 5
         suggested += Math.round(10 * Math.random()) - 5;
-        return { suggested: suggested, min: min, max: max, private: qdata }
-    }
-    /**
-    * Determine what to tell the user about how the score has been computed
-    * @param suggesteddata Data calculated for the score range
-    * @returns HTML String to display in the suggested question dialog
-    */
-    public genSamplePointsText(suggesteddata: suggestedData): string {
-        const qdata = suggesteddata.private as QuoteRecord
-        let result = ''
+        let text = ''
         let rangetext = ''
-        if (suggesteddata.max > suggesteddata.min) {
-            rangetext = ` (From a range of ${suggesteddata.min} to ${suggesteddata.max})`
+        if (max > min) {
+            rangetext = ` (From a range of ${min} to ${max})`
         }
         if (qdata.len < 15) {
-            result = `<p><b>WARNING:</b> <em>There are only ${qdata.len} characters in the quote, we recommend at least 20 characters for a good quote</em></p>`
+            text = `<p><b>WARNING:</b> <em>There are only ${qdata.len} characters in the quote, we recommend at least 20 characters for a good quote</em></p>`
         }
         if (qdata.len > 2) {
-            result += `<p>There are ${qdata.len} characters in the quote, ${qdata.unique} of which are unique.
-             We suggest you try a score of ${suggesteddata.suggested}${rangetext}</p>`
+            text += `<p>There are ${qdata.len} characters in the quote, ${qdata.unique} of which are unique.
+             We suggest you try a score of ${suggested}${rangetext}</p>`
         }
-        return result
+        return { suggested: suggested, min: min, max: max, text: text }
     }
     /**
      * Generate the HTML to display the answer for a cipher

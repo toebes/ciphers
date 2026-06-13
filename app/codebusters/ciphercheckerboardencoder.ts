e24a545043c5d905642de720c67fcc9b912711b5
nibarder <nibarder1@gmail.com>
2026-06-13 11:05:59 -0400

    remove extra space between lines (#802)

--- app/codebusters/ciphercheckerboardencoder.ts
+++ app/codebusters/ciphercheckerboardencoder.ts
@@ -2687,9 +2687,9 @@ export class CipherCheckerboardEncoder extends CipherEncoder {
         for (const sequenceset of strings) {
             const rowcipher = table.addBodyRow();
             const blankrow1 = table.addBodyRow();
-            const blankrow2 = table.addBodyRow();
+            //const blankrow2 = table.addBodyRow();
             blankrow1.add("\u00A0");
-            blankrow2.add("\u00A0");
+            //blankrow2.add("\u00A0");
             for (const token of sequenceset[source]) {
                 rowcipher.add(token);
             }
